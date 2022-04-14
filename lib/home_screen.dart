c8a70637341d84973a40b4bcb153c33583e247e6
Aliaksei_Meshcharakou <alex84grodno@mail.ru>
2022-04-14 21:23:15 +0300

    add webView

---
+++ lib/home_screen.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MyWebView extends StatelessWidget {
+  final String title;
+  final String selectedUrl;
+
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  MyWebView({
+    Key? key,
+    required this.title,
+    required this.selectedUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: WebView(
+        initialUrl: selectedUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Open Webpage"),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => MyWebView(
+                  title: "Marvel",
+                  selectedUrl: "https://developer.marvel.com/documentation/images",
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
